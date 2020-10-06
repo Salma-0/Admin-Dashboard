52d841a1d87622ca244cf622b0ed2afbaaa03154
Salma <salma--95@hotmail.com>
2020-10-07 01:28:56 +0300

    Updates

---
+++ precache-manifest.8df1f9d8576f4ba6a68e590f3cc676ee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ddc00a888942ef8f0bf365fb04d137a",
+    "url": "/Admin-Dashboard/index.html"
+  },
+  {
+    "revision": "9b63e4498942b20a78f3",
+    "url": "/Admin-Dashboard/static/css/3.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5242f17b71a89a5a34d8",
+    "url": "/Admin-Dashboard/static/css/main.7f38d273.chunk.css"
+  },
+  {
+    "revision": "9b63e4498942b20a78f3",
+    "url": "/Admin-Dashboard/static/js/3.bec6745a.chunk.js"
+  },
+  {
+    "revision": "a1dd73de632d3c9cf39d7872e4c706cb",
+    "url": "/Admin-Dashboard/static/js/3.bec6745a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f1000c12c113dceeb0",
+    "url": "/Admin-Dashboard/static/js/4.a3cfb71f.chunk.js"
+  },
+  {
+    "revision": "6d667652cd0b35de4cd1",
+    "url": "/Admin-Dashboard/static/js/5.5fae2e39.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/Admin-Dashboard/static/js/5.5fae2e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f077bddd0a4cd206a8",
+    "url": "/Admin-Dashboard/static/js/6.eb29e38f.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/Admin-Dashboard/static/js/6.eb29e38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5242f17b71a89a5a34d8",
+    "url": "/Admin-Dashboard/static/js/main.0339ffa4.chunk.js"
+  },
+  {
+    "revision": "cef59048c79cd76194e1",
+    "url": "/Admin-Dashboard/static/js/runtime-main.387914b6.js"
+  },
+  {
+    "revision": "d7aaff1b9c70a39deca3",
+    "url": "/Admin-Dashboard/static/js/xlsx.f637f1a2.chunk.js"
+  }
+]);
---
+++ static/js/xlsx.f637f1a2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[2],{324:function(n,c){},411:function(n,c){},412:function(n,c){}}]);
+//# sourceMappingURL=xlsx.f637f1a2.chunk.js.map
